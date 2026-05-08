159afb7691bc37a212950ceb49aaf096a963c37c
Silvio Donizeti De Mattos Junior <Silvio.donizeti.jr@outlook.com>
2026-05-08 09:05:45 -0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Signum/',
   plugins: [react()],
   resolve: {
     // Guarantee a single copy of React in the bundle
